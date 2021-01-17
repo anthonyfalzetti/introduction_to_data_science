1928d6b292614a8db05ca6d2be267fe5f2d3b275
Anthony Falzetti <anthony.falzetti@gmail.com>
2021-01-17 04:36:20 -0700

    Add week 1 python assignments

---
+++ week_1/intro_exercise.py
@@ -0,0 +1,33 @@
+# Lines starting with # are comments and are not run by Python.
+"""
+Multi-line comments are possible with triple quotes like this.
+"""
+# 
+# import pandas and matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# This data comes from the UCI ML repository:
+# https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
+# It is the daily number of users from a bike share program
+df = pd.read_csv('day.csv')
+
+# shows a preview of the data
+df.head()
+# shows some basic stats of the data
+df.describe()
+
+# Use the examples in the jupyter notebook to help you here.
+# calculate the mean and standard deviation of the hourly data counts (the 'cnt' column)
+# mean
+df.cnt.mean
+# standard deviation
+df.cnt.std
+# plot the counts ('cnt' column)
+plt.plot(df.cnt)                  
+plt.xlabel('hourly data counts')  
+plt.title('Bike Share')           
+plt.show()                        
+
+# Carry out other EDA and analysis if you wish for more practice.
+# You can also carry this out in a Jupyter Notebook.
